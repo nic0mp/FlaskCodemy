729c07c6b57f2cf771df215106e6a397adec51f5
nic0mp <nico.mp215@gmail.com>
2021-12-12 10:08:10 -0600

    static dir and name.html

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # create a Flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my secret key nobody needs to know'
+
+# Create form class
+class NamerForm(FlaskForm):
+    name = StringField('Whats your name', validators=[DataRequired()])
+    submit =  SubmitField('Submit')
 
 # create a route decorator
 @app.route('/')
@@ -39,3 +48,18 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+# Create Name page
+@app.route('/name', methods=['GET','POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash('Form submitted successfully')
+    return render_template('name.html',
+    name = name,
+    form = form
+    )
