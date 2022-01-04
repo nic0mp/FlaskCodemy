dd76366b2dc7366dd54eaf69741b07891e9f8f26
nic0mp <nico.mp215@gmail.com>
2022-01-04 16:34:57 -0600

    migrate db

--- hello.py
+++ hello.py
@@ -19,6 +19,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
+    favorite_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
     # Create a string
@@ -29,6 +30,7 @@ class Users(db.Model):
 class UserForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
+    favorite_color = StringField("Favorite Color")
     submit =  SubmitField('Submit')
 
 # Update database record
@@ -39,6 +41,7 @@ def update(id):
     if request.method == "POST":
         name_to_update.name = request.form['name']
         name_to_update.email = request.form['email']
+        name_to_update.favorite_color = request.form['favorite_color']
         try:
             db.session.commit()
             flash('User updated successfully!')
@@ -71,12 +74,13 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data)
+            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
+        form.favorite_color.data = ''
         flash('User added')
     our_users = Users.query.order_by(Users.date_added)
     return render_template('add_user.html',form=form,name=name,our_users=our_users)
